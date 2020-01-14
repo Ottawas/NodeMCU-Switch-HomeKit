e5fb535a449d7b9d3c5f5bdd955b73c08fda73d1
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-01-14 19:53:34 +0100

    Change code for HomeKit_Switch

--- HomeKit_IR_LedStrip_GITHUB.ino
+++ HomeKit_IR_LedStrip_GITHUB.ino
@@ -1,6 +1,6 @@
 /* Nom du projet: Apple Homebridge – MQTT IR REMOTE
    GitHub: https://github.com/Ottawas
-   Description: Permet de simuler une lumière sur HomeKit et d'envoyer un signal infrarouge à la place.
+   Description: Permet de simuler un SWITCH sur HomeKit et d'envoyer un signal infrarouge à la place.
    Projet terminé le: 26/07/2019 21:05
    Version: 0.1.0
    License: MIT
@@ -13,16 +13,19 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-const char* ssid = "SSID DU WIFI"; //SSDI de votre WiFi.
-const char* password = "MOT DE PASSE DU WIFI"; //Mot de passe de votre WiFi.
+const char* ssid = "SSDI DU WIFI"; //SSDI de votre WiFi
+const char* password = "MOT DE PASSE DU WIFI"; //Mot de passe de votre WiFi
 const char* mqtt_server = "192.168.0.X";
 
 /*
-Votre serveur MQQT, comment l'installer ? Voici un tutoriel: https://www.studiopieters.nl/homebridge-homekit-2-mqtt/
-mqtt_server = "l'adresse ip du raspberry"
+  Votre serveur MQQT, comment l'installer ? Voici un tutoriel: https://www.studiopieters.nl/homebridge-homekit-2-mqtt/
+  mqtt_server = "l'adresse ip du raspberry/machine"
 */
 
-const uint16_t ir_led = 4; //On déclare la led infrarouge (4 = D2).
+
+int STATE;
+
+const uint16_t ir_led = 4; //On déclare la led infrarouge (4 = D2)
 IRsend irsend(ir_led);
 
 WiFiClient espClient;
@@ -53,19 +56,39 @@ void setup_wifi() {
 
 void callback(char* topic, byte* payload, unsigned int length) {
 
-  //Pour comprendre tout ceci je vous conseille de vous renseignez sur les topics MQTT (il faut du temps pour comprendre, mais après c'est facile).
+  if (String(topic) == "switch/bedroom/bureau/state") {     //SI le topic qui répond est "switch/bedroom/bureau/state":
+    for (int i = 0; i < length; i++) {                       //On décode la réponse.
+      payload[length] = '\0';                                //On la transforme en String puis en Integer.
+      String s = String((char*)payload);                     //On la transforme en String puis en Integer.
+      int HomeKit_State = s.toInt();                         //On la transforme en String puis en Integer.
 
-    if (String(topic) == "hue/set/lights/bureau"){      //SI le topic qui répond est "hue/set/lights/bureau":
-      for (int i = 0; i < length; i++) {                //On décode la réponse.
-        payload[length] = '\0';                         //On la transforme en String puis en Integer.
-        String s = String((char*)payload);              //On la transforme en String puis en Integer.
-        int state = s.toInt();                          //On la transforme en String puis en Integer.
-        irsend.sendNEC(0xFF02FD, 32);                   //On envoit une requête infrarouge.
-        //Serial.println("Requête");
-          }
-      }
+      
+      if (HomeKit_State == 0 && STATE == 1) {    //DONC AVANT ALLUME
+        irsend.sendNEC(0xFF02FD, 32);                     //Envoyer code infrarouge.
+ 
+        Serial.print("\n[INFO] Statut: ");
+        Serial.print(HomeKit_State);
+        STATE = 0;
+        }                                                 //MAINTENANT ETEINT
 
+
+      else if (HomeKit_State == 1 && STATE == 0) {    //DONC AVANT ETEINT
+        irsend.sendNEC(0xFF02FD, 32);                     //Envoyer code infrarouge.
+        
+        Serial.print("\n[INFO] Statut: ");
+        Serial.print(HomeKit_State);
+        STATE = 1;
+        }                                                  //MAINTENANT ALLUM
+         
+    }
   }
+  
+  else {
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+  }
+}
 
 
 
@@ -79,14 +102,14 @@ void reconnect() {
 
     //Tentative de connexion.
     if (client.connect(clientId.c_str())) {
-      Serial.println("\n[HomeKit] Connexion au serveur réussi !");
+      Serial.println("\n[HomeKit] Connexion au serveur réussie !");
 
       //Connexion réussie, on s'abonne aux topics réglés sur homekit2mqtt.
-      client.subscribe("hue/set/lights/bureau");
+      client.subscribe("switch/bedroom/bureau/+"); //METTRE VOTRE CONFIG HOMEKIT2MQTT
 
 
     } else {
-      Serial.print("failed, rc=");
+      Serial.print("impossible, rc=");
       Serial.print(client.state());
       Serial.println(" nouvelle tentative dans 5 secondes...");
       delay(5000);
@@ -97,22 +120,26 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
-  irsend.begin(); //On initialise la fonction pour l'infrarouge.
+
+  
+  irsend.begin(); //On itinie la fonction pour l'infrarouge.
 
   setup_wifi();
-  ArduinoOTA.setHostname("ESP-LED"); //Nom pour reconaissance OTA (Over The Air).
-  ArduinoOTA.setPassword("led"); //Mot de passe pour sécurisation.
-  ArduinoOTA.begin(); //On initialise l'OTA.
+  
+  ArduinoOTA.setHostname("ESP-SWITCH"); //Nom pour reconaissance OTA (Over The Air)
+  ArduinoOTA.setPassword("VOTRE MDP"); //Mot de passe pour sécurisation.
+  ArduinoOTA.begin(); //On initialise l'OTA
+  
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+
 }
 
 void loop() {
-
   if (!client.connected()) {
     reconnect();
   }
   ArduinoOTA.handle();
   client.loop();
-
- }
+}
